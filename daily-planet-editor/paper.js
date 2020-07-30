647a152664732169fe72c542c49db6d3c43237e6
Braflee <brad.duffy24@gmail.com>
2020-07-30 12:56:20 -0700

    20200730

---
+++ daily-planet-editor/paper.js
@@ -0,0 +1,38 @@
+var enemyName = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
+
+funtion Who_Wins(hasKryptonite, enemyName) {
+    if (!hasKryptonite) {
+        console.log("Superman beats " + enemyName + ", of course");
+    } else {
+        console.log("Depends on how quick Superman can get rid of the Kryptonite. " + enemyname + " could possibly win this one.");
+    }
+}
+
+for (var i = 0; i < enemies.length; i++) {
+    var hasKryptonite:
+        if (i % 2 === 0) {
+        hasKryptonite = true;
+    } else {
+        hasKryptonite = false;
+    }
+    console.log(Who_Wins(hasKryptonite, enemies[i]));
+}
+
+function HowAttractedIsLoisLaneToMe() {
+    console.log(HowAttractedIsLoisLaneToMe());{
+        return Math.floor((math.random() * 10) + 1);
+    }
+}
+
+
+var clarkKent = true;
+var superman = false;
+var phoneBoothQuickChange = math.random();
+while (clarkKent === true) {
+    console.log("I'm just a nerdy columnist");
+    if (phoneBoothQuickChange >= 0.5) {
+        clarkKent = false;
+        superman = true;
+        console.log("Now I'm Superman!");
+    }
+}
