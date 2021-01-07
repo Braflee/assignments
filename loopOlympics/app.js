74540df9c72992368bf46e21fd6966c624cd13bd
Braflee <brad.duffy24@gmail.com>
2021-01-07 15:10:28 -0800

    20200107

---
+++ loopOlympics/app.js
@@ -0,0 +1,34 @@
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+// for(let i = numbers.length -1; i >= 0; i--){
+//     console.log(numbers[i])
+// }
+
+ var fruit = ["banana", "orange", "apple", "kiwi"]
+
+//  for(let i = 0; i < fruit.length; i++){
+//     console.log(fruit[i])
+// }
+
+for (let i = 0; i < fruit.length; i += 2) {
+    console.log(fruit[i])
+}
+
+let ghostArr = []
+
+// for(var i = 0; i < 9; i++){
+//     console.log(ghostArr.push(i))
+// }
+
+var evenArr = []
+
+// for(let i = 0; i <= 100; i++){
+//     if(i % 2 === 0){
+//         evenArr.push(i)
+//     }
+// }
+// console.log(evenArr)
