f706ef8450451865aff530fb3856f59c689312eb
Braflee <brad.duffy24@gmail.com>
2021-02-09 14:16:25 -0800

    20210209pt2

--- htmlCalc/app.js
+++ htmlCalc/app.js
@@ -1,7 +1,12 @@
-// const form = document.getElementsByName('additionForm')
+const addForm = document["additionForm"];
 
-addEventListener('submit', function name(params) {
-    
-} )
+addForm.addEventListener("submit", function(event){
+    event.preventDefault()
 
+    const num1 = addForm.add1.value
+    const num2 = addForm.add2.value
 
+    let newSum = num1 + num2
+    console.log(newSum)
+
+})
