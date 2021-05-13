e1fc6b4169faa7a82805055a0dfb2f3112b9accf
Braflee <brad.duffy24@gmail.com>
2021-05-13 13:08:38 -0700

    20210513

--- lvl-2/errHandle-prac/app.js
+++ lvl-2/errHandle-prac/app.js
@@ -1,11 +1,47 @@
+
 function sum(x, y){
-    return x + y
+    x = Number(x)
+    y = Number(y)
+        if(x === '') {
+            throw 'No number inserted'
+        } else if(isNaN(x)) {
+            throw 'not a number'
+        } else if(y === '') {
+            throw 'No number inserted'
+        } else if(isNaN(y)) {
+            throw 'not a number'
+        } else {
+            return x + y
+        }
+
 }
 
-try{
-    sum(1, 2)
-    throw new Error('test')
+try {
+    console.log(sum(1, 2))
 }
 catch(err){
     console.log(err)
+}
+finally{
+    console.log('...is this thing on?')
+}
+
+const credentials = {username: 'Braflee', password: 'haha'}
+
+function login(username, password){
+    if(username == 'Braflee' && password == 'haha'){
+        console.log('Logged in.')
+    } else {
+        throw 'Wrong'
+    }
+}
+
+try {
+    console.log(login('Braflee', 'haha'))
+}
+catch(err){
+    console.log(err)
+}
+finally{
+    console.log('I gotchu boo.')
 }
