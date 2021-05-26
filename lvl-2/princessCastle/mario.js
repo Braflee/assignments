0dcca4ffdf911805596bf8b7440951437bff9e8d
Braflee <brad.duffy24@gmail.com>
2021-05-25 20:30:14 -0700

    20210525

--- lvl-2/princessCastle/mario.js
+++ lvl-2/princessCastle/mario.js
@@ -1,79 +1,82 @@
 const readlineSync = require('readline-sync');
-const counter = setInterval(() => fate(roll), 1500)
+let gameActive = true
 
-class Player {
-    constructor(name, totalCoins, status, hasStar=false, gameActive=true){
-        this.name, 
-        this.totalCoins, 
-        this.status,
-        this.hasStar,
-        this.gameActive
-    }
-    setName(chooseFighter){
-        chooseFighter = readlineSync.keyIn('Who do you think deserves to find Peach? [m]Mario, or [l]Luigi?', {limit: 'ml'})
-            if(chooseFighter === 'm'){
-                this.name = 'Mario'
-                fate()
-            } else if(chooseFighter === 'l'){
-                this.name = 'Luigi'
-                fate()
-            }else{
-                console.log('Nooooooo pick one or the other')
+if(gameActive === true){
+    game()
+} else if(gameActive === false){
+    console.log('dead')
+}
+function game(){
+    class Player {
+        constructor(name, totalCoins, status, hasStar=false){
+            this.name = name,  
+            this.totalCoins = totalCoins,
+            this.status = status,
+            this.hasStar = hasStar
+        }
+        setName(namePicked){
+            setName = readlineSync.keyIn('Who do you think deserves to find Peach? [m]Mario, or [l]Luigi?', {limit: 'ml'})
+            if (namePicked === 'm'){
+                    this.name = 'Mario'
+            } else if (namePicked === 'l'){
+                    this.name = 'Luigi'
+                }else{
+                    console.log('Nooooooo pick one or the other')
+                }
+            }    
+        gotHit(){
+            if(this.status === 'Powered Up'){
+                this.status = 'Big'
+                console.log('Your star protected you!')
+            }else if(this.status === 'Big'){
+                this.status = 'Smol'
+            }else if(this.status === 'Smol'){
+                this.status === 'DED'
+                gameActive === false
             }
-        }    
-    gotHit(){
-        if(this.status === 'Powered Up'){
-            this.status = 'Big'
-        }else if(this.status === 'Big'){
-            this.status = 'Smol'
-        }else if(this.status === 'Smol'){
-            this.status = 'DED'
-            this.gameActive = false
-            clearInterval(counter)
+        }  
+        gotPowerUp(){
+            if(this.status === 'Smol'){
+                this.status = 'Big'
+                this.hasStar = false
+            }else if(this.status === 'Big'){
+                this.status = 'Powered Up'
+                this.hasStar = true
+            }else{
+                console.log('Bro chill you have the star already')
+            }
+        }   
+        gotCoin(){
+            this.totalCoins += 1;
         }
-    }  
-    gotPowerUp(){
-        if(this.status === 'Smol'){
-            this.status = 'Big'
-        }else if(this.status === 'Big'){
-            this.status = 'Powered Up'
-            this.hasStar = true
-        }else{
-            console.log('Bro chill you have the star already')
-        }
-    }   
-    gotCoin(){
-        this.totalCoins += 1;
-    }
-    print(){
-        console.log(`Brother: ${this.name}`)
-        console.log(`Bank: ${this.totalCoins} coins`)
-        console.log(`Power Status: ${this.status}!`)
-        if(this.hasStar === true){
-            console.log(`You have the star!`)
+        print(){
+            console.log(`Brother: ${this.name}`)
+            console.log(`Bank: ${this.totalCoins} coins`)
+            console.log(`Power Status: ${this.status}!`)
+            if(this.hasStar === true){
+                console.log(`You have the star!`)
+            } else {
+                console.log(`No star`)
+            }
         }
     }
+    const marioBro = new Player('Mario', 0, 'Smol')
+    setInterval(() => {
+        const randomNum = Math.floor(Math.random() * 3)
+        marioBro.print()
+        console.log(randomNum)
+        if (randomNum === 0) {
+            marioBro.gotHit()
+        } else if (randomNum === 1) {
+            marioBro.gotPowerUp()
+        } else if(randomNum === 2) {
+            marioBro.gotCoin()
+        } 
+    }, 1000)
 }
 
-const marioBro = new Player('test', 0, 'Smol')
 
-function rng(min, max){
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
 
-let roll = rng(0, 2);
 
-function fate(roll){
-    marioBro.setName()
-    print()
-    if(roll === 0){
-        marioBro.gotHit()
-    }else if(roll ===  1){
-        marioBro.gotPowerUp()
-    }else{
-        marioBro.gotCoin()
-    }
-}
+
 
