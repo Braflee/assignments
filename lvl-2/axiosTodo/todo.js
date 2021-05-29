9ea4e98ea5d7deca4354524e86134b98d42a3f72
Braflee <brad.duffy24@gmail.com>
2021-05-28 17:22:29 -0700

    20210528

--- lvl-2/axiosTodo/todo.js
+++ lvl-2/axiosTodo/todo.js
@@ -1,24 +1,53 @@
-// The user can see their current list of todos.
-// Todos show up as soon as the page loads.
-// If a todo item is complete, it should have a strikethrough line on it
-
-const { default: axios } = require("axios");
-const todoForm = document.todoForm
-// Images should be displayed as images if there are any
 function getData(){
-    axios.get("https://api.vschool.io/<brad-duffy>/todo/")
+    axios.get("https://api.vschool.io/bradduffy/todo")
         .then(res => listData(res.data))
         .catch(err => console.log(err))
 }
 
 function listData(data){
+    clearList()
     for(let i = 0; i < data.length; i++){
-        const h1 = document.createElement('h2')
-        h1.textContent = data[i].title
-        document.getElementById('todoBody').appendChild(h1)
+        const titl = document.createElement('h1')
+        titl.textContent = data[i].title
+        document.getElementById('todoList').appendChild(titl)
+        
+        const desc = document.createElement('h3')
+        desc.textContent = data[i].description
+        document.getElementById('todoList').appendChild(desc)
+
+        const pri = document.createElement('h3')
+        pri.textContent = data[i].price
+        document.getElementById('todoList').appendChild(pri)
     }
 }
 
 function clearList(){
-    
+    const el = document.getElementById('todoList')
+    while(el.firstChild){
+        el.removeChild(el.firstChild)
+    }
 }
+
+getData()
+
+const todoForm = document.todoForm
+todoForm.addEventListener("submit", function (e){
+    e.preventDefault()
+
+    const newTodo = {
+       title: todoForm.title.value,
+       description: todoForm.description.value,
+       price: todoForm.price.value,
+       imgUrl: todoForm.imgUrl.value
+
+    } 
+
+    console.log("The title input is" + todoForm.title.value)
+
+    axios.post("https://api.vschool.io/bradduffy/todo", newTodo)
+        .then(res => getData())
+        .catch(err => console.log(err))
+    
+    todoForm.title.value = ""
+
+})
