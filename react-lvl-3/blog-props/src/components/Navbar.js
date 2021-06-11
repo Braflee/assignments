2475a778eb16eee270d4f21cbb5dd2ba070223c8
Braflee <brad.duffy24@gmail.com>
2021-06-10 18:24:56 -0700

    20210610

---
+++ react-lvl-3/blog-props/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar(){
+    return (
+        <div className='navCont'>
+            <div className='brand'>
+                <p>Start Bootstrap</p>
+            </div>
+            <div className='fLinks'>
+                <p>Home</p>
+                <p>About</p>
+                <p>Sample Post</p>
+                <p>Contact</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
